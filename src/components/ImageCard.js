40a14792c862f22765c4f77fbde009431a0c15e3
SAURABH SINGH <62793814+saurabhsingh-7@users.noreply.github.com>
2023-08-13 20:41:36 +0530

    Update ImageCard.js

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,6 +3,22 @@ import React from 'react';
 const ImageCard = ({ image }) => {
   const tags = image.tags.split(',');
 
+  const shareImage = () => {
+    const shareText = `Check out this amazing photo by ${image.user} on Pixabay!`;
+    const shareUrl = image.webformatURL;
+
+    // Use the Web Share API to share the image
+    if (navigator.share) {
+      navigator.share({
+        title: shareText,
+        text: shareText,
+        url: shareUrl,
+      })
+      .then(() => console.log('Shared successfully'))
+      .catch((error) => console.error('Error sharing:', error));
+    }
+  };
+
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
       <img src={image.webformatURL} alt="" className="w-full"/>
@@ -32,8 +48,22 @@ const ImageCard = ({ image }) => {
         </span>
         ))}
       </div>
+      <div className="px-6 py-4 flex justify-between">
+        <button onClick={shareImage} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Share
+        </button>
+        {/* Add share buttons for other platforms */}
+        {/* Add Pinterest, Facebook, Twitter, and WhatsApp share buttons */}
+        {/* You can use respective share URLs for each platform */}
+        {/* Example: */}
+         <a href={`https://pinterest.com/pin/create/button/?url=${encodeURIComponent(image.webformatURL)}&media=${encodeURIComponent(image.webformatURL)}&description=${encodeURIComponent('Check out this amazing photo by ' + image.user + ' on Pixabay!')}`} target="_blank" rel="noopener noreferrer">
+          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+            Pin It
+          </button>
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default ImageCard;
